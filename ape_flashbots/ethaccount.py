c0b4312381917efd7662adfdd9f55a346b4a398d
fubuloubu <fubuloubu@users.noreply.github.com>
2026-09-23 23:12:19 -0400

    chore: align packaging with ApeWorX/alchemy standard (golden: ape-polygon)

--- ape_flashbots/ethaccount.py
+++ ape_flashbots/ethaccount.py
@@ -1,33 +1,30 @@
-from eth_account.signers.local import LocalAccount
-from web3.middleware import construct_sign_and_send_raw_middleware
-
-from flashbots import flashbot
-from flashbots import FlashbotProvider
-from flashbots.types import SignTx
-from eth_account.account import Account
-from web3 import Web3, HTTPProvider
-from web3.types import TxParams, Wei
-from web3.middleware import geth_poa_middleware
-
 from decouple import config
+from eth_account.account import Account
+from eth_account.signers.local import LocalAccount
+from flashbots import flashbot
+from flashbots.types import SignTx
+from web3 import HTTPProvider, Web3
+from web3.middleware import construct_sign_and_send_raw_middleware, geth_poa_middleware
+from web3.types import TxParams, Wei
+
 """
 In this example we setup a transaction for 0.1 eth with a gasprice of 1
 From here we will use Flashbots to pass a bundle with the needed content
 """
 
-from web3 import Web3
 from eth_account import Account, messages
 
 
 class EthAccount:
-
     def __init__(self):
         self.eth_account_signature = Account.from_key(config("ETH_SIGNATURE_KEY"))
         self.eth_account_from: LocalAccount = Account.from_key(config("ETH_PRIVATE_FROM"))
         self.eth_account_to: LocalAccount = Account.from_key(config("ETH_PRIVATE_TO"))
         self.body = '{"id": 1234, "method", "eth_sendBundle", "params": [["0x123..."], "0xB84969"]}'
         self.message = messages.encode_defunct(text=Web3.keccak(text=self.body).hex())
-        self.signed_message = Account.sign_message(self.message, private_key=self.eth_account_signature.key)
+        self.signed_message = Account.sign_message(
+            self.message, private_key=self.eth_account_signature.key
+        )
         self.w3 = Web3(HTTPProvider("https://relay-goerli.flashbots.net"))
         self.w3.middleware_onion.inject(geth_poa_middleware, layer=0)
         self.bribe = self.w3.toWei("0.0001", "ether")
@@ -54,7 +51,9 @@ class EthAccount:
             "nonce": self.w3.eth.get_transaction_count(self.eth_account_from.address),
         }
         try:
-            tx = self.w3.eth.send_transaction(params,)
+            tx = self.w3.eth.send_transaction(
+                params,
+            )
             print("Request sent! Waiting for receipt")
         except ValueError as e:
             # Skipping if TX already is added and pending
@@ -94,7 +93,9 @@ class EthAccount:
     def complete_transaction(self):
         self.send_request()
         bundle = self.flashbot_request()
-        result = self.w3.flashbots.send_bundle(bundle, target_block_number=self.w3.eth.blockNumber + 3)
+        result = self.w3.flashbots.send_bundle(
+            bundle, target_block_number=self.w3.eth.blockNumber + 3
+        )
         result.wait()
         receipts = result.receipts()
         block_number = receipts[0].blockNumber
